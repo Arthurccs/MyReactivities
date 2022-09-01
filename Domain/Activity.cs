3abea137678abd5ab76f03de2c690b9d56217aef
Carlos Ruiz <carlos.ruiz@projekt202.com>
2022-09-01 11:52:30 -0500

    Adding new entity Activity, representation for activities table

---
+++ Domain/Activity.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Domain
+{
+
+    /// <summary>
+    /// Class for entity Activity
+    /// </summary>
+    public  class Activity
+    {
+        /// <summary>
+        /// unique Id of the activity
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Title of the activity
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Date of the activity
+        /// </summary>
+        public DateTime DAte { get; set; }
+
+        /// <summary>
+        /// Description of the activity
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Category of the activity
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// City of the activity
+        /// </summary>
+        public string City { get; set; }
+
+
+        /// <summary>
+        /// Venue of the activity
+        /// </summary>
+        public string Venue { get; set; }
+    }
+}
