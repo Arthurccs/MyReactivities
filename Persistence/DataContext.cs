8d64ea7428647f3b09cd8be6abe43db2e4fa6cf5
Carlos Ruiz <carlos.ruiz@projekt202.com>
2022-12-26 01:59:17 -0500

    adding data Context

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    /// <summary>
+    /// represent a session with the DB 
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
